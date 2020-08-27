22d8da03ce34c43694b70ae276073e20dc12bbff
CharlesWang <charleswang@taiwanmobile.com>
2020-08-27 17:44:26 +0800

    [update] add test project

---
+++ SDKCarthageTest/SDKCarthageTest/HelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.swift
+//  SDKCarthageTest
+//
+//  Created by Charles on 2020/8/27.
+//  Copyright © 2020 Taiwanmobile. All rights reserved.
+//
+
+import Foundation
+
+class HelloWorld {
+    func show() {
+        print("Hi there!!!")
+    }
+}
---
+++ SDKCarthageTest/SDKCarthageTest/SDKCarthageTest.h
@@ -0,0 +1,19 @@
+//
+//  SDKCarthageTest.h
+//  SDKCarthageTest
+//
+//  Created by Charles on 2020/8/27.
+//  Copyright © 2020 Taiwanmobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKCarthageTest.
+FOUNDATION_EXPORT double SDKCarthageTestVersionNumber;
+
+//! Project version string for SDKCarthageTest.
+FOUNDATION_EXPORT const unsigned char SDKCarthageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKCarthageTest/PublicHeader.h>
+
+
